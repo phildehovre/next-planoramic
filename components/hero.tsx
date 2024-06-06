bf94e73d3fd8b9d877d32bbf0014765c3cf92943
Philippe De Hovre <ph.dehovre@gmail.com>
2024-06-06 10:46:36 +0100

    Green gsap test removed

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,65 +6,65 @@ import "./hero.css";
 import { gsap } from "gsap";
 
 const hero = () => {
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+  // useLayoutEffect(() => {
+  //   gsap.registerPlugin(ScrollTrigger);
 
-    const ctx = gsap.context(() => {
-      const details = gsap.utils.toArray(
-        ".desktopContentSection:not(:first-child)"
-      );
-      const photos: GSAPStaggerVars[] = gsap.utils.toArray(
-        ".desktopPhoto:not(:first-child)"
-      );
+  //   const ctx = gsap.context(() => {
+  //     const details = gsap.utils.toArray(
+  //       ".desktopContentSection:not(:first-child)"
+  //     );
+  //     const photos: GSAPStaggerVars[] = gsap.utils.toArray(
+  //       ".desktopPhoto:not(:first-child)"
+  //     );
 
-      gsap.set(photos, { yPercent: 101 });
+  //     gsap.set(photos, { yPercent: 101 });
 
-      const allPhotos: GSAPStaggerVars[] = gsap.utils.toArray(".desktopPhoto");
+  //     const allPhotos: GSAPStaggerVars[] = gsap.utils.toArray(".desktopPhoto");
 
-      // create
-      let mm = gsap.matchMedia();
+  //     // create
+  //     let mm = gsap.matchMedia();
 
-      // add a media query. When it matches, the associated function will run
-      mm.add("(min-width: 600px)", () => {
-        // this setup code only runs when viewport is at least 600px wide
-        console.log("desktop");
+  //     // add a media query. When it matches, the associated function will run
+  //     mm.add("(min-width: 600px)", () => {
+  //       // this setup code only runs when viewport is at least 600px wide
+  //       console.log("desktop");
 
-        ScrollTrigger.create({
-          trigger: ".gallery",
-          start: "top top",
-          end: "bottom bottom",
-          pin: ".right",
-          // markers: true,
-        });
+  //       ScrollTrigger.create({
+  //         trigger: ".gallery",
+  //         start: "top top",
+  //         end: "bottom bottom",
+  //         pin: ".right",
+  //         // markers: true,
+  //       });
 
-        //create scrolltrigger for each details section
-        //trigger photo animation when headline of each details section
-        //reaches 80% of window height
-        details.forEach((detail: any, index) => {
-          let headline = detail.querySelector("h1");
-          let animation = gsap
-            .timeline()
-            .to(photos[index], { yPercent: 0 })
-            .set(allPhotos[index], { autoAlpha: 0 });
-          ScrollTrigger.create({
-            trigger: headline,
-            start: "top 70%",
-            end: "top 50%",
-            animation: animation,
-            scrub: true,
-            // markers: true,
-          });
-        });
+  //       //create scrolltrigger for each details section
+  //       //trigger photo animation when headline of each details section
+  //       //reaches 80% of window height
+  //       details.forEach((detail: any, index) => {
+  //         let headline = detail.querySelector("h1");
+  //         let animation = gsap
+  //           .timeline()
+  //           .to(photos[index], { yPercent: 0 })
+  //           .set(allPhotos[index], { autoAlpha: 0 });
+  //         ScrollTrigger.create({
+  //           trigger: headline,
+  //           start: "top 70%",
+  //           end: "top 50%",
+  //           animation: animation,
+  //           scrub: true,
+  //           // markers: true,
+  //         });
+  //       });
 
-        return () => {
-          // optional
-          // custom cleanup code here (runs when it STOPS matching)
-          console.log("mobile");
-        };
-      });
-    });
-    return () => ctx.revert();
-  }, []);
+  //       return () => {
+  //         // optional
+  //         // custom cleanup code here (runs when it STOPS matching)
+  //         console.log("mobile");
+  //       };
+  //     });
+  //   });
+  //   return () => ctx.revert();
+  // }, []);
 
   return (
     <div className="w-screen flex justify-center overflow-x-hidden h-[1000vh] ">
